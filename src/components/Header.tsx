133a9fda82869ff7e6f206fd336cb6bd26192e96
drazedco <drazedstore@gmail.com>
2025-08-11 18:43:17 +0500

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -85,7 +85,7 @@ const Header = () => {
           <img
             src="/images/logo.png"
             alt="Millat Polymer Logo"
-            className="w-48 lg:w-50 object-contain"
+            className="w-60 lg:w-50 object-contain"
           />
         </Link>
 
